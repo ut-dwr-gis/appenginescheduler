1b65f5ea3d21cf825642098e6d9f47a793c7d544
Tyler <127980137+TjStrat@users.noreply.github.com>
2024-10-02 14:33:32 -0600

    Add files via upload

---
+++ env.py
@@ -0,0 +1,8 @@
+import oracledb
+conn = oracledb.connect(
+            user='biotics_report',
+            password='JBu.K6~VaLhggPL4',
+            host='bioticsut.natureserve.org',
+            port=1521,
+            service_name='biotics'
+      )
